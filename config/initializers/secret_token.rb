706b19777eef802266cb2904cef22c2411b80a0e
Bennett <potatosalad@mc384b-1.c.byui.edu>
2011-02-24 11:02:34 -0700

    Initial git import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pagination::Application.config.secret_token = 'd6f74298b243ee1dedf649ac057a18b7747a7a4868f80d83abe64fa2f6abe8dfb07a740b2c48d161b4282d3373d3e93a68a2edf3eb42ec6125cedc359a5e21a7'
